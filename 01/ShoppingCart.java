08d40b624198e3eca53fb0d32cee1f7eb6a4ad0e
cediackermann <ackermanncedi@gmail.com>
2026-04-21 11:17:54 +0200

    init

---
+++ 01/ShoppingCart.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+
+    private List<String> items = new ArrayList<String>();
+    private boolean bookAdded;
+
+    public static void main(String[] args) {}
+}
