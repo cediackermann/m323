b71f6978f15c03ecbb3a5cf3ee48c84e222afe28
cediackermann <ackermanncedi@gmail.com>
2026-06-13 00:03:36 +0200

    small update to LeetCode

--- Sources/LeetCode/LeetCode.swift
+++ Sources/LeetCode/LeetCode.swift
@@ -14,7 +14,7 @@ enum LeetCode {
     }
 
     static func sortedSquares(_ nums: [Int]) -> [Int] {
-        nums.map { $0  $0 }.sorted()
+        nums.map { $0 * $0 }.sorted()
     }
 
     static func productExceptSelf(_ nums: [Int]) -> [Int] {
@@ -47,16 +47,23 @@ enum LeetCode {
     }
 }
 
-
-  public class TreeNode {
-      public var val: Int
-      public var left: TreeNode?
-      public var right: TreeNode?
-      public init() { self.val = 0; self.left = nil; self.right = nil; }
-      public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-      public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-          self.val = val
-          self.left = left
-          self.right = right
-      }
-  }
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() {
+        self.val = 0
+        self.left = nil
+        self.right = nil
+    }
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
